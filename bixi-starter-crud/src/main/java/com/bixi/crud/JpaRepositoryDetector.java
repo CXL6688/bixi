54ba451d7f60d73df2b236d09ec3ab02dab3f5eb
曹学亮 <18333601856@163.com>
2020-08-22 19:41:37 +0800

    初始化项目

--- bixi-starter-crud/src/main/java/com/bixi/crud/JpaRepositoryDetector.java
+++ bixi-starter-crud/src/main/java/com/bixi/crud/JpaRepositoryDetector.java
@@ -3,26 +3,43 @@ package com.bixi.crud;
 import com.bixi.util.BixiReflectionUtil;
 import org.springframework.aop.TargetSource;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaMetamodelEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
-public class JpaRepositoryDetector {
-    @Autowired
-    private List<JpaRepository> jpaRepositories;
+public class JpaRepositoryDetector implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
 
     public void detect(){
-        List<String> customEntityNames= this.detectAllCustomEntiryNames();
+        List<String> customEntityNames= this.detectAllEntiryNamesWithCustomRepository();
+
     }
 
-    private List<String> detectAllCustomEntiryNames() {
+    private List<String> getAllEntityNamesInSpringContainer(){
+        List<String> entityNames=new ArrayList<>();
+        Map<String, Object> map =  this.applicationContext.getBeansWithAnnotation(Entity.class);
+        map.values().stream().forEach(entity->{
+            Entity entityAnnotation= entity.getClass().getAnnotation(Entity.class);
+
+        });
+    }
+
+    private List<String> detectAllEntiryNamesWithCustomRepository() {
+        Map<String,JpaRepository> jpaRepositoryMap= this.applicationContext.getBeansOfType(JpaRepository.class);
+        List<JpaRepository> jpaRepositories = jpaRepositoryMap.values().stream().collect(Collectors.toList());
         List<String> custormEntityNames=new ArrayList<>();
         jpaRepositories.stream().forEach(jpaRepository -> {
             Proxy proxy=(Proxy)jpaRepository;
@@ -44,7 +61,8 @@ public class JpaRepositoryDetector {
         return custormEntityNames;
     }
 
-    public void detect1(){
-
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
     }
 }
